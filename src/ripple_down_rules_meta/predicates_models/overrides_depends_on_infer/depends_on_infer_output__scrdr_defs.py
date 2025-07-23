27f2920f35cb8f59932f33407574d4d01d53056b
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-07-23 18:15:38 +0200

    [RDR] yield first commit

--- src/ripple_down_rules_meta/predicates_models/overrides_depends_on_infer/depends_on_infer_output__scrdr_defs.py
+++ src/ripple_down_rules_meta/predicates_models/overrides_depends_on_infer/depends_on_infer_output__scrdr_defs.py
@@ -1,4 +1,4 @@
-from typing_extensions import Type
+from typing_extensions import Type, Tuple, Generator
 
 from ripple_down_rules import has, dependsOn
 from ripple_down_rules.datastructures.tracked_object import TrackedObjectMixin
@@ -17,18 +17,20 @@ def conditions_3929361033208322153670901849644072096(case) -> bool:
     return conditions_for_depends_on_evaluate(**case)
 
 
-def conclusion_3929361033208322153670901849644072096(case) -> bool:
+def conclusion_3929361033208322153670901849644072096(case) \
+        -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
     def depends_on_evaluate(cls_: Type[DependsOn], dependent: Type[TrackedObjectMixin],
-                            dependency: Type[TrackedObjectMixin], recursive: bool, **kwargs) -> bool:
+                            dependency: Type[TrackedObjectMixin], recursive: bool, **kwargs) \
+            -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
         """Get possible value(s) for depends_on_evaluate.output_  of type ."""
-        return has(owner_type=dependent, member_type=dependency, recursive=recursive)
+        yield from has(dependent, dependency, recursive=recursive)
 
     return depends_on_evaluate(**case)
 
 
 def conditions_114740124515448400708123236287653930408(case) -> bool:
     def conditions_for_depends_on_evaluate(cls_: Type[DependsOn], dependent: Type[TrackedObjectMixin],
-                                           dependency: Type[TrackedObjectMixin], recursive: bool,
+                                           dependency: Type[TrackedObjectMixin], recursive: bool, is_reversed: bool,
                                            **kwargs) -> bool:
         """Get conditions on whether it's possible to conclude a value for depends_on_evaluate.output_  of type ."""
         return isinstance(dependent, Rule) and isinstance(dependency, Rule)
@@ -36,14 +38,15 @@ def conditions_114740124515448400708123236287653930408(case) -> bool:
     return conditions_for_depends_on_evaluate(**case)
 
 
-def conclusion_114740124515448400708123236287653930408(case) -> bool:
+def conclusion_114740124515448400708123236287653930408(case) \
+        -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
     def depends_on_evaluate(cls_: Type[DependsOn], dependent: Type[TrackedObjectMixin],
-                            dependency: Type[TrackedObjectMixin], recursive: bool, **kwargs) -> bool:
+                            dependency: Type[TrackedObjectMixin], recursive: bool, is_reversed: bool, **kwargs) \
+            -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
         """Get possible value(s) for depends_on_evaluate.output_  of type ."""
-        return any(dependsOn(dependent_ct, dependency_ct, recursive=recursive) for dependent_ct in
-                   dependent.conclusion.conclusion_type for
-                   dependency_ct in dependency.conclusion.conclusion_type if
-                   issubclass(dependent_ct, TrackedObjectMixin) and issubclass(dependency_ct,
-                                                                               TrackedObjectMixin))
+        yield from (dependsOn(dependent_ct, dependency_ct, recursive=recursive, is_reversed=is_reversed)
+                    for dependent_ct in dependent.conclusion.conclusion_type
+                    for dependency_ct in dependency.conclusion.conclusion_type
+                    if issubclass(dependent_ct, TrackedObjectMixin) and issubclass(dependency_ct, TrackedObjectMixin))
 
     return depends_on_evaluate(**case)
