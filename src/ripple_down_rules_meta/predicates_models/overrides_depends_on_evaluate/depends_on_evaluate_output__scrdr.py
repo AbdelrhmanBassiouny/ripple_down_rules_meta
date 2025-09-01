2151964896c357216c46e49c2f8f1df64c394c35
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-09-01 23:42:38 +0200

    [RDRPredicate] merged changes from the yield branch.

--- src/ripple_down_rules_meta/predicates_models/overrides_depends_on_evaluate/depends_on_evaluate_output__scrdr.py
+++ src/ripple_down_rules_meta/predicates_models/overrides_depends_on_evaluate/depends_on_evaluate_output__scrdr.py
@@ -1,25 +1,26 @@
 from types import NoneType
 from ripple_down_rules.datastructures.case import Case, create_case
 from typing_extensions import Optional, Dict
-from .depends_on_evaluate_output__scrdr_defs import *
+from .depends_on_infer_output__scrdr_defs import *
 
 
 attribute_name = 'output_'
-conclusion_type = (bool,)
+conclusion_type = (Tuple[Type[TrackedObjectMixin, TrackedObjectMixin]],)
 mutually_exclusive = True
 name = 'output_'
 case_type = Dict
 case_name = 'depends_on_evaluate'
 
 
-def classify(case: Dict, **kwargs) -> Optional[bool]:
+def classify(case: Dict, **kwargs)\
+        -> Generator[Tuple[Type[TrackedObjectMixin], Type[TrackedObjectMixin]], None, None]:
     if not isinstance(case, Case):
         case = create_case(case, max_recursion_idx=3)
 
     if conditions_3929361033208322153670901849644072096(case):
-        return conclusion_3929361033208322153670901849644072096(case)
+        yield from conclusion_3929361033208322153670901849644072096(case)
 
     elif conditions_114740124515448400708123236287653930408(case):
-        return conclusion_114740124515448400708123236287653930408(case)
+        yield from conclusion_114740124515448400708123236287653930408(case)
     else:
-        return None
+        raise StopIteration
